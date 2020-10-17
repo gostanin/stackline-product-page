70a22f871a504601813d2ded90a14d2642fa7c3e
Gregory Ostanin <ostingreg@gmail.com>
2020-10-16 22:12:42 -0700

    Add ProductCard to a Product page

--- src/containers/Product.js
+++ src/containers/Product.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import styles from "./Product.module.css";
 import Chart from "../components/Statistics/Chart/Chart";
+import ProductCard from "../components/ProductCard/ProductCard";
 
 const product = (props) => {
     return (
         <div className={styles.Product}>
+            <ProductCard />
             <Chart />
         </div>
     );
