bd3e7fa7b3850b107c8dcc1f62ad68a43fcf2cca
Gregory Ostanin <ostingreg@gmail.com>
2020-10-17 16:50:13 -0700

    Connect product card to redux store

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import styles from "./ProductCard.module.css";
 import Tags from "./Tags/Tags";
 import ProductInformation from "./ProductInformation/ProductInformation";
 import NavigationMenu from "../Navigation/NavigationMenu/NavigationMenu";
 
-const productCard = (props) => {
-    return (
-        <div className={styles.ProductCard}>
-            <ProductInformation />
-            <Tags />
-            <NavigationMenu />
-        </div>
-    );
+class ProductCard extends Component {
+    render() {
+        return (
+            <div className={styles.ProductCard}>
+                <ProductInformation
+                    title={this.props.title}
+                    subtitle={this.props.subtitle}
+                    imgLink={this.props.image}
+                />
+                <Tags tags={this.props.tags} />
+                <NavigationMenu />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        tags: state.tags,
+        title: state.title,
+        subtitle: state.subtitle,
+        image: state.image,
+    };
 };
 
-export default productCard;
+export default connect(mapStateToProps)(ProductCard);
