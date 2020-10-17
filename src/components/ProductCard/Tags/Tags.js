408449d5b8dbac6100cf27a00988cb97cb49a5bd
Gregory Ostanin <ostingreg@gmail.com>
2020-10-17 16:13:54 -0700

    Update tags component to use props

--- src/components/ProductCard/Tags/Tags.js
+++ src/components/ProductCard/Tags/Tags.js
@@ -3,14 +3,10 @@ import React from "react";
 import styles from "./Tags.module.css";
 
 const tags = (props) => {
-    return (
-        <div className={styles.Tags}>
-            <p>Pantry</p>
-            <p>Obselete</p>
-            <p>Blender</p>
-            <p>Lighting Deal</p>
-        </div>
-    );
+    const tags = props.tags.map((tag) => {
+        return <p>{tag}</p>;
+    });
+    return <div className={styles.Tags}>{tags}</div>;
 };
 
 export default tags;
