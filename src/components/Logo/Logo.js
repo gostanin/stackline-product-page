0617a0bf7c2fe33b3ae6f23e5489a54dbdb6c57d
Gregory Ostanin <ostingreg@gmail.com>
2020-10-16 21:42:09 -0700

    Add Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import logoImg from "../../assets/logo.png";
+
+const logo = (props) => {
+    return (
+        <div>
+            <img src={logoImg} alt="Stackline" />
+        </div>
+    );
+};
+
+export default logo;
