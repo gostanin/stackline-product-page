aea8092cd2e86dbc0580141c07dd1282dfc9295c
Gregory Ostanin <ostingreg@gmail.com>
2020-10-17 15:57:34 -0700

    Connect redux store and product component

--- src/containers/Product.js
+++ src/containers/Product.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import styles from "./Product.module.css";
+import * as actions from "../store/actions";
 import Chart from "../components/Statistics/Chart/Chart";
 import ProductCard from "../components/ProductCard/ProductCard";
 
-const product = (props) => {
-    return (
-        <div className={styles.Product}>
-            <ProductCard />
-            <Chart />
-        </div>
-    );
+class Product extends Component {
+    componentDidMount() {
+        this.props.loadData();
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <div className={styles.Product}>
+                <ProductCard />
+                <Chart items={this.props.sales}/>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return { sales: state.sales };
 };
 
-export default product;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        loadData: () => dispatch(actions.loadData()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
