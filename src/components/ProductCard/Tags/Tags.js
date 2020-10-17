ad8f70b53e26c61f45e0c391c13446be110a0c83
Gregory Ostanin <ostingreg@gmail.com>
2020-10-17 16:50:28 -0700

    Update tags component to work with props

--- src/components/ProductCard/Tags/Tags.js
+++ src/components/ProductCard/Tags/Tags.js
@@ -3,8 +3,8 @@ import React from "react";
 import styles from "./Tags.module.css";
 
 const tags = (props) => {
-    const tags = props.tags.map((tag) => {
-        return <p>{tag}</p>;
+    const tags = props.tags.map((tag, index) => {
+        return <p key={index}>{tag}</p>;
     });
     return <div className={styles.Tags}>{tags}</div>;
 };
